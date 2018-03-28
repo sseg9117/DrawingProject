813bcb934e1ea7927949ab5a3eea0bb081517708
Sheradon Segota <31513874+sseg9117@users.noreply.github.com>
2018-03-28 08:41:02 -0600

    added controllers and classes with jframe .

---
+++ src/art/view/ArtPanel.java
@@ -0,0 +1,16 @@
+package art.view;
+
+import javax.swing.JPanel;
+
+import art.controller.ArtController;
+
+public class ArtPanel extends JPanel
+{
+	private final int MINIMUM_EDGE = 5;
+	private final int MAXIMUM_EDGE = 20;
+	private final int MINIMUM_SCALE = 20;
+	private final int MAXIMUM_SCALE = 100;
+	
+	private ArtController app;
+	
+}
