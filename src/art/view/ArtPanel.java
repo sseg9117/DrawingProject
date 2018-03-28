dd605c24157473400942937a24674d30c40e1ff9
Sheradon Segota <31513874+sseg9117@users.noreply.github.com>
2018-03-28 09:43:32 -0600

    did stuff.

--- src/art/view/ArtPanel.java
+++ src/art/view/ArtPanel.java
@@ -66,42 +66,6 @@ public class ArtPanel extends JPanel
 		setupLayout();
 		setupListeners();
 	}
-
-	private void setupListeners()
-	{
-		
-	}
-
-	private void setupLayout()
-	{
-		
-	}
-
-	private void setupPanel()
-	{
-		this.setLayout(appLayout);
-		this.setBackground(Color.DARK_GRAY);
-		this.setPreferredSize(new Dimension(1024, 768));
-		this.add(canvas);
-		
-		buttonPanel.setPreferredSize(new Dimension(200, 450));;
-		buttonPanel.add(triangleButton);
-		buttonPanel.add(rectangleButton);
-		buttonPanel.add(ellipseButton);
-		buttonPanel.add(polygonButton);
-		buttonPanel.add(clearButton);
-		buttonPanel.add(saveButton);
-		buttonPanel.add(colorButton);
-		
-		sliderPanel.setPreferredSize(new Dimension(250, 450));;
-		sliderPanel.add(scaleSlider);
-		sliderPanel.add(edgeSlider);
-		
-		this.add(buttonPanel);
-		this.add(sliderPanel);
-		
-	}
-
 	private void setupSliders()
 	{
 		Hashtable<Integer, JLabel> scaleLabels = new Hashtable<Integer, JLabel>();
@@ -129,4 +93,42 @@ public class ArtPanel extends JPanel
 		edgeSlider.setPaintTicks(true);
 		edgeSlider.setPaintLabels(true);
 	}
+	
+	private void setupPanel()
+	{
+		this.setLayout(appLayout);
+		this.setBackground(Color.DARK_GRAY);
+		this.setPreferredSize(new Dimension(1024, 768)); // Used for display in WindowBuilder
+		this.add(canvas);
+		
+		buttonPanel.setPreferredSize(new Dimension(200, 450));;
+		buttonPanel.add(triangleButton);
+		buttonPanel.add(rectangleButton);
+		buttonPanel.add(ellipseButton);
+		buttonPanel.add(polygonButton);
+		buttonPanel.add(clearButton);
+		buttonPanel.add(saveButton);
+		buttonPanel.add(colorButton);
+		
+		sliderPanel.setPreferredSize(new Dimension(250, 450));;
+		sliderPanel.add(scaleSlider);
+		sliderPanel.add(edgeSlider);
+		
+		this.add(buttonPanel);
+		this.add(sliderPanel);	
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
+
+
+
+
 }
--- src/art/view/ShapeCanvas.java
+++ src/art/view/ShapeCanvas.java
@@ -1,6 +1,23 @@
 package art.view;
 
-public class ShapeCanvas
-{
+import java.awt.Polygon;
+import java.awt.Rectangle;
+import java.awt.geom.Ellipse2D;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 
+import javax.swing.JPanel;
+
+import art.controller.ArtController;
+
+public class ShapeCanvas extends JPanel
+{
+	private ArrayList<Polygon> triangleList;
+	private ArrayList<Polygon> polygonList;
+	private ArrayList<Ellipse2D> ellipseList;
+	private ArrayList<Rectangle> rectangleList;
+	private ArtController app;
+	
+	private BufferedImage canvasImage;
+	
 }
