c81e2c0e223dbf17619c6ca52afb6272213f4f7e
Sheradon Segota <31513874+sseg9117@users.noreply.github.com>
2018-03-28 09:12:39 -0600

    did setupSLider.

--- src/art/view/ArtPanel.java
+++ src/art/view/ArtPanel.java
@@ -1,6 +1,13 @@
 package art.view;
 
+import java.awt.GridLayout;
+import java.util.Hashtable;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.SpringLayout;
 
 import art.controller.ArtController;
 
@@ -13,4 +20,91 @@ public class ArtPanel extends JPanel
 	
 	private ArtController app;
 	
+	private SpringLayout appLayout;
+	private ShapeCanvas canvas;
+	private JPanel buttonPanel;
+	private JPanel sliderPanel;
+	private JSlider scaleSlider;
+	private JSlider edgeSlider;
+	private JButton triangleButton;
+	private JButton rectangleButton;
+	private JButton ellipseButton;
+	private JButton polygonButton;
+	private JButton saveButton;
+	private JButton clearButton;
+	private JButton colorButton;
+	
+	private int currentEdgeCount;
+	private int currentScale;
+	
+	public ArtPanel(ArtController app)
+	{
+		super();
+		this.app = app;
+		appLayout = new SpringLayout();
+		
+		currentScale = MINIMUM_SCALE;
+		currentEdgeCount = MINIMUM_EDGE;
+		scaleSlider = new JSlider(MINIMUM_SCALE, MAXIMUM_SCALE);
+		edgeSlider = new JSlider(MINIMUM_EDGE, MAXIMUM_EDGE);
+		
+		canvas = new ShapeCanvas(app);
+		sliderPanel = new JPanel();
+		buttonPanel = new JPanel(new GridLayout(0, 1));
+		
+		triangleButton = new JButton("add triangle");
+		rectangleButton = new JButton("add rectangle");
+		ellipseButton = new JButton("add ellipse");
+		polygonButton = new JButton("add polygon");
+		clearButton = new JButton("clear image");
+		colorButton = new JButton("change color");
+		
+		setupSliders();
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+
+	private void setupListeners()
+	{
+		
+	}
+
+	private void setupLayout()
+	{
+		
+	}
+
+	private void setupPanel()
+	{
+		
+	}
+
+	private void setupSliders()
+	{
+		Hastable<Integer, JLabel> scaleLabels = new Hashtable<Integer, JLabel>();
+		Hastable<Integer, JLabel> edgeLabels = new Hashtable<Integer, JLabel>();
+		
+		scaleLabels.put(MINIMUM_SCALE, new JLabel("<HTML>Small<BR>Shape</HTML>"));
+		scaleLabels.put((MAXIMUM_SCALE + MINIMUM_SCALE) / 2, new JLabel("<HTML>Small<BR>Shape</HTML>"));
+		scaleLabels.put(MAXIMUM_SCALE, new JLabel("<HTML>Large<BR>Shape</HTML>"));
+		
+		edgeLabels.put(MINIMUM_EDGE, new JLabel("Edges: " + MINIMUM_EDGE));
+		edgeLabels.put(MAXIMUM_EDGE, new JLabel("Edges: " + MAXIMUM_EDGE));
+		
+		scaleSlider.setLabelTable(scaleLabels);
+		scaleSlider.setOrientation(JSlider.VERTICAL);
+		scaleSlider.setSnapToTicks(true);
+		scaleSlider.setMajorTickSpacing(10);
+		scaleSlider.setPaintLabels(true);
+		scaleSlider.setPaintLabels(true);
+		
+		edgeSlider.setLabelTable(edgeLabels);
+		edgeSlider.setOrientation(JSlider.VERTICAL);
+		edgeSlider.setSnapToTicks(true);
+		edgeSlider.setMajorTickSpacing(3);
+		edgeSlider.setMinorTickSpacing(1);
+		edgeSlider.setPaintTicks(true);
+		edgeSlider.setPaintLabels(true);
+	}
 }
